0eedb956f0b07f9fdc17dcd82959e0ffd4575d7e
PierreReis <pierre.rdo17@gmail.com>
2024-09-14 21:30:14 -0300

    Desafio LandingPage

---
+++ Desafio-LandingPage/JS/index.js
@@ -0,0 +1,10 @@
+var nome = window.document.getElementById('nome')
+var email = window.document.getElementById('email')
+function alterarCorNome() {
+    nome.style.color = 'black'
+    nome.style.transition = '0.5s'
+}
+function alterarCorEmail() {
+    email.style.color = 'black'
+    email.style.transition = '0.5s'
+}
